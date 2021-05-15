58b930ee2a0df0318fc7bf8c81ee44f4b11e4367
rorocode <replees62@gmail.com>
2021-05-14 20:28:00 -0700

    Added answer system
    Added answer system with checker to script and UI. main feature set complete

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ solfege = ["ti","do","ra","re","me","mi","fa","fi","so","le","la","te","ti","do"
 #set up settings
 numOptions = [2,3,4,5,6,7,8,9]
 chromOptions = [0,1,2,3,4,5,6,7,8,9]
-answer=""
 #generate audio and answers
 def generate():
     #reset answer
+    global answer
     answer = ""
+    feedback.set("Enter your answer")
+    answerFeedback.set("")
     #get settings
     numPref=numPick.get()
     chromPref=chromPick.get()
@@ -53,24 +55,39 @@ def playKey():
     play(key)
 def playMix():
     play(mix)
-
+def checkAnswer():
+    inputanswer=answerField.get()
+    guess = inputanswer.replace(" ","")
+    truth = answer.replace(" ","")
+    answerField.delete(0, tk.END)
+    if truth == guess:
+        feedback.set("Correct!")
+    else:
+        feedback.set("Incorrect!")
+        answerFeedback.set("Answer: "+answer)
 #setup window
 window = tk.Tk()
+window.title("SolfegeEarTrain")
 window.resizable(False,False)
 #setup tkinter variables
 numPick = tk.IntVar()
+feedback = tk.StringVar()
+answerFeedback = tk.StringVar()
 chromPick = tk.IntVar()
 numPick.set(3)
 chromPick.set(1)
-
+feedback.set("Enter your answer")
 numMenu = tk.OptionMenu(window, numPick, *numOptions)
 chromMenu = tk.OptionMenu(window, chromPick, *chromOptions)
-generateButton = tk.Button(window,text="new chord",command=generate)
-keyButton = tk.Button(window,text="play key",command = playKey)
-chordButton = tk.Button(window,text="play chord",command = playMix)
+generateButton = tk.Button(window,text="New Chord",command=generate)
+keyButton = tk.Button(window,text="Play Key",command = playKey)
+chordButton = tk.Button(window,text="Play Chord",command = playMix)
 numLabel = tk.Label(window,text="Notes:")
 chromLabel = tk.Label(window,text="Chromatics:")
-
+enterButton = tk.Button(window,text="Enter",command=checkAnswer)
+answerField = tk.Entry(window)
+feedbackText = tk.Label(window,textvar=feedback,width="15")
+answerText = tk.Label(window,textvar=answerFeedback)
 
 
 
@@ -83,18 +100,20 @@ if __name__ == '__main__':
     numLabel.grid(column=1,row=1, sticky='nesw')
     numMenu.grid(column=2,row=1, sticky='nesw')
     chromLabel.grid(column=3,row=1, sticky='nesw')
-    chromMenu.grid(column=4,row=1, sticky='nesw')
+    chromMenu.grid(column=4, row=1, sticky='nesw')
     generateButton.grid(column=6,row=1, sticky='nesw')
     keyButton.grid(column=6,row=3, sticky='nesw')
     chordButton.grid(column=6,row=4, sticky='nesw')
-
-
+    enterButton.grid(column=4, row=3, sticky='nsew')
+    answerField.grid(column=2,row=3,sticky='nsw',columnspan=2)
+    feedbackText.grid(column=2,row=4,columnspan=2, sticky='nsew')
+    answerText.grid(column=2, row=5, columnspan=2)
     for i in range(0,6):
         window.grid_columnconfigure(i, weight=1, minsize=30)
     window.grid_columnconfigure(100, weight=1, minsize=30)
     for i in range(0,5):
        window.grid_rowconfigure(i, weight=1, minsize=30)
-    window.grid_rowconfigure(100, weight=1, minsize=30)
+    window.grid_rowconfigure(100, weight=1, minsize=10)
     generate()
     window.mainloop()
 
